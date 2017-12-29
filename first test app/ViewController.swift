2b47ae7ebf7424ee31fc07c8e4b50eb58ec81897
Omar Rahman <omarrahman96@gmail.com>
2017-12-29 01:15:43 +0000

    Enable winning and restart logic.

--- first test app/ViewController.swift
+++ first test app/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
     
     var cross: Bool = true
     let buttonTextPlaceholder = "Play me!"
-    var played: [Bool] = [Bool](repeating: false, count: 9)
+    var crossPlay: [Bool?] = [Bool?](repeating: nil, count: 9)
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +29,7 @@ class ViewController: UIViewController {
 
     @IBAction func playTurn(_ sender: UIButton)
     {
-        if(!played[sender.tag])
+        if(crossPlay[sender.tag] == nil)
         {
             let img = cross ? UIImage(named: "ic_close_3x.png") : UIImage(named: "ic_radio_button_unchecked_3x.png")
             sender.setImage(img, for: UIControlState.normal)
@@ -36,17 +37,32 @@ class ViewController: UIViewController {
             let backgorundColor = cross ? UIColor.red : UIColor.blue
             sender.tintColor = backgorundColor
             cross = !cross
-            played[sender.tag] = true
+            crossPlay[sender.tag] = cross ? true : false
+            winningCombinations.forEach({ (combination) in
+                if crossPlay[combination[0]] != nil && crossPlay[combination[0]] == crossPlay[combination[1]] && crossPlay[combination[1]] == crossPlay[combination[2]]
+                {
+                    let alert = UIAlertController(title: "Congratulations", message: "You Have Won!", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "Restart", style: UIAlertActionStyle.default, handler:
+                        {(alert: UIAlertAction!) in self.reset()}
+                    ))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            })
+            
         }
     }
     
     @IBAction func resetGrid(_ sender: Any) {
+        reset()
+    }
+    
+    func reset(){
         collectionOfButtons?.forEach({ (button) in
             button.setTitle(buttonTextPlaceholder, for: UIControlState.normal)
             button.setImage(nil, for: UIControlState.normal)
             button.tintColor = nil
         })
-        played = [Bool](repeating: false, count: 9)
+        crossPlay = [Bool?](repeating: nil, count: 9)
         cross = true
     }
 }
