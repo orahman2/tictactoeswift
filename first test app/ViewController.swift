dccb510bfbaf78696f6cfa8d7bb67a02cfcce55a
Omar Rahman <omarrahman96@gmail.com>
2017-12-29 00:30:40 +0000

    Initial commit. UI fully functional.

--- first test app/ViewController.swift
+++ first test app/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var collectionOfButtons: Array<UIButton>?
+    
+    var cross: Bool = true
+    let buttonTextPlaceholder = "Play me!"
+    var played: [Bool] = [Bool](repeating: false, count: 9)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func playTurn(_ sender: UIButton)
+    {
+        if(!played[sender.tag])
+        {
+            let img = cross ? UIImage(named: "ic_close_3x.png") : UIImage(named: "ic_radio_button_unchecked_3x.png")
+            sender.setImage(img, for: UIControlState.normal)
+            sender.setTitle("", for: UIControlState.normal)
+            let backgorundColor = cross ? UIColor.red : UIColor.blue
+            sender.tintColor = backgorundColor
+            cross = !cross
+            played[sender.tag] = true
+        }
+    }
+    
+    @IBAction func resetGrid(_ sender: Any) {
+        collectionOfButtons?.forEach({ (button) in
+            button.setTitle(buttonTextPlaceholder, for: UIControlState.normal)
+            button.setImage(nil, for: UIControlState.normal)
+            button.tintColor = nil
+        })
+        played = [Bool](repeating: false, count: 9)
+        cross = true
+    }
 }
 
